5b232d1f2a522bc3117fdff71f05b75edd208cec
codecustard <emmanuelbarroga@gmail.com>
2021-09-08 14:52:54 -0400

    Initial commit, setup firebase, register, login

---
+++ front/src/utils/FirebaseConnector.js
@@ -0,0 +1,78 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+const InitializeFirebase = () => {
+    // TODO: Replace the following with your app's Firebase project configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyBeU2p7rpAu1nUiWYlbV4R7y9A1CYtoMiw",
+        authDomain: "gekchio.firebaseapp.com",
+        projectId: "gekchio",
+        storageBucket: "gekchio.appspot.com",
+        messagingSenderId: "545190063381",
+        appId: "1:545190063381:web:b23b157c7dc9c00450cd1c"
+    };
+
+    console.log(`InitializeFirebase`);
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+}
+
+const CreateUserWithEmailAndPassword = async  (email, password) => {
+    // EXAMPLE:
+    // InitializeFirebase();
+    // let newUser = {};
+    // CreateUserWithEmailAndPassword('test4@email.com', 'password').then( (user) => {
+    //   newUser = user;
+    //   console.log(`User - ${JSON.stringify(newUser)}`);
+    // });
+
+    const auth = getAuth();
+    let returnValue = {};
+
+    let user = await createUserWithEmailAndPassword(auth, email, password)
+                        .catch( (error) => {
+                            const errorCode = error.code;
+                            const errorMessage = error.message;
+                            
+                            console.log(`Error - ${error}`);
+                            returnValue = error;
+                            return returnValue;
+                        });
+    return user;
+}
+
+const SignInWithEmailAndPassword = async (email, password) => {
+    //EXAMPLE:
+    // InitializeFirebase();
+    // CreateUserWithEmailAndPassword(...)
+    // ...
+    // let loginUser = {};
+    // SignInWithEmailAndPassword('test4@email.com', 'password').then( (user) => {
+    //   loginUser = user;
+    //   console.log(`User - ${JSON.stringify(loginUser)}`);
+    // });
+
+    const auth = getAuth();
+    let returnValue = {};
+
+    let user = await signInWithEmailAndPassword(auth, email, password)
+                        .catch( (error) => {
+                            const errorCode = error.code;
+                            const errorMessage = error.message;
+                            
+                            console.log(`Error - ${error}`);
+                            returnValue = error;
+                            return returnValue;
+                        });
+    return user;
+}
+
+export {
+    InitializeFirebase,
+    CreateUserWithEmailAndPassword,
+    SignInWithEmailAndPassword
+};
