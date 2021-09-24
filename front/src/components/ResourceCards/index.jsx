11c862b1cb6651f3727b7bd325e38a439d39433d
Emmanuel Barroga <emmanuelbarroga@gmail.com>
2021-09-23 23:51:46 -0400

    Initial commit game description modal
    Game Description Modal

--- front/src/components/ResourceCards/index.jsx
+++ front/src/components/ResourceCards/index.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./index.css"
 
+import { GlobalContext } from '../../contexts/GlobalContext';
+
 const ResourceCard = (props) => {
+
+    const [context, setContext] = useContext(GlobalContext);
+
     console.log(props.key, "resource card")
+
+    const onDetailsClick = () => {
+
+        setContext( (prevContext) => {
+            return {
+                ...prevContext, 
+                gameDescriptionModal: {
+                    game: props.game,
+                    description: props.description,
+                    author: props.author,
+                    genre: props.genre,
+                    platform: props.platform,
+                    download_link: props.download_link,
+                    images: props.images,
+                    
+                    show: true
+                }
+            }
+        });
+    }
     
     return (
         
             <div className="resource-text">    
-                        <div>
-                            <a href="<%= resource.link %>">   
-                              <img className="game-preview" src="/images/ resource.image "/> </a>
+                        <div> 
+                              <img className="game-preview" src={props.images[0]}/>
                         </div> 
                         <span className="emphasis"> {props.game} </span><br></br> {props.description} 
                         <br></br>
@@ -18,7 +42,7 @@ const ResourceCard = (props) => {
                         {props.platform.map((e) => {
                             return e + ' '
                         })}
-                        <a className="all-images__link" href="resource.details">Details</a>
+                        <a className="all-images__link" onClick={() => {onDetailsClick()}}>Details</a>
               </div>
         
     )
